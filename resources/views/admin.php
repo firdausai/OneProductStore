4a577ef00d4212fa0a5615be3fa491283c20d585
firdausai <firdaus.indradhirmaya@gmail.com>
2020-05-02 21:21:13 +0700

    edit

--- resources/views/admin.php
+++ resources/views/admin.php
@@ -154,7 +154,7 @@
             </div>
         </main>
         <footer id="footer-wrapper" class="footer-wrapper text-white text-center">
-            2020 Millenis Corporation. All rights reserved
+            2020 Store Name. All rights reserved
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
